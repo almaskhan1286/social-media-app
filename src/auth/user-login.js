de8c9282031895e84374d7b461a9c64ccf85dc65
almaskhan1286 <muhammadalmas1286@gmail.com>
2024-05-02 17:40:31 +0500

    "minor changes"

--- src/auth/user-login.js
+++ src/auth/user-login.js
@@ -20,7 +20,7 @@ function handleUserLogin(username, password) {
         const token = data.token;
         localStorage.setItem('token', token);
         alert('SignIn Successfully.');
-        window.location.replace('.............');
+        window.location.replace('../src/main.html');
     })
     .catch(error => {
         console.error('Error during SignIn: ', error.message)
